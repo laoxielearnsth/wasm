0efbea140f74b93d50cf25ee823043b3bca9c746
v_yanghhxie <v_yanghhxie@tencent.com>
2020-12-13 18:12:35 +0800

    feat: 使用canvas完成conway-life-game

--- site/index.js
+++ site/index.js
@@ -1,18 +1,83 @@
 const js = import("../pkg/hello_wasm.js");
+const bg_js = import("../pkg/hello_wasm_bg.wasm");
 
-js.then(js=>{
-    const {Universe} = js;
-    const pre = document.getElementById("game-of-life-canvas");
+Promise.all([js, bg_js])
+    .then((res) => {
+        const {Universe, Cell} = res[0];
+        const {memory} = res[1];
 
-    const universe = Universe.new();
+        const CELL_SIZE = 5;
+        const GRID_COLOR = "#CCC";
+        const DEAD_COLOR = "#FFF";
+        const ALIVE_COLOR = "#000"
 
-    const renderLoop = () => {
-        pre.textContent = universe.render();
-        universe.tick();
+        const universe = Universe.new();
+        const width = universe.get_width();
+        const height = universe.get_height();
+
+// const pre = document.getElementById("game-of-life-canvas");
+        let canvas = document.getElementById('canvas');
+        canvas.height = (CELL_SIZE + 1) * height + 1;
+        canvas.width = (CELL_SIZE + 1) * width + 1;
+        let ctx = canvas.getContext('2d');
+
+        const drawGrid = () => {
+            ctx.beginPath();
+            ctx.strokeStyle = GRID_COLOR;
+
+            // Vertical lines.
+            for (let i = 0; i <= width; i++) {
+                ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+                ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+            }
+
+            // Horizontal lines.
+            for (let j = 0; j <= height; j++) {
+                ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+                ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+            }
+
+            ctx.stroke();
+        };
+
+        const getIndex = (row, col) => {
+            return row * width + col;
+        };
+
+        const drawCells = () => {
+            const cellsPtr = universe.get_cells();
+            const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+            ctx.beginPath();
+
+            for (let row = 0; row < height; row++) {
+                for (let col = 0; col < width; col++) {
+                    const idx = getIndex(row, col)
+
+                    ctx.fillStyle = cells[idx] === Cell.Dead
+                        ? DEAD_COLOR
+                        : ALIVE_COLOR;
+
+                    ctx.fillRect(
+                        col * (CELL_SIZE + 1) + 1,
+                        row * (CELL_SIZE + 1) + 1,
+                        CELL_SIZE,
+                        CELL_SIZE
+                    )
+                }
+            }
+
+            ctx.stroke();
+        };
+
+        const renderLoop = () => {
+            universe.tick();
+
+            drawGrid();
+            drawCells();
+
+            requestAnimationFrame(renderLoop);
+        };
 
         requestAnimationFrame(renderLoop);
-    };
-
-    requestAnimationFrame(renderLoop);
-});
-
+    });
